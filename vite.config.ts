f2834b6f6c28fce4e2ddb47c900aaa5088bce084
AzatF2020 <AzatGaynutdinov2006@yandex.ru>
2024-10-02 15:28:01 +0300

    Добавил пакеты для unit тестиварония + файловая структуа modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      core: path.resolve(__dirname, './src/core'),
+      modules: path.resolve(__dirname, './src/modules'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['legacy-js-api'],
+      },
+    },
+  },
+  server: {
+    port: 3001,
+  },
+});
